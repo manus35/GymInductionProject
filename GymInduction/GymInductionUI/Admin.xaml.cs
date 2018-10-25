45449a34dfc34f89c113de0417070e330c2ec694
dermo <l00092109@student.lyit.ie>
2018-10-25 19:25:01 +0100

    added page for admin, deleted admin

--- GymInduction/GymInductionUI/Admin.xaml.cs
+++ GymInduction/GymInductionUI/Admin.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace GymInductionUI
@@ -17,7 +18,7 @@ namespace GymInductionUI
     /// <summary>
     /// Interaction logic for Admin.xaml
     /// </summary>
-    public partial class Admin : Window
+    public partial class Admin : Page
     {
         public Admin()
         {
